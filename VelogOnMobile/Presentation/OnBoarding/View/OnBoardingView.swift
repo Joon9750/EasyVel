5313cf55bfe3408be7cab097d0319decd94d243c
Hongjunehuke <junhyeok2166@daum.net>
2023-05-11 17:28:37 +0900

    [ADD] OnBoarding View 파일 생성

---
+++ VelogOnMobile/Presentation/OnBoarding/View/OnBoardingView.swift
@@ -0,0 +1,13 @@
+//
+//  OnBoardingView.swift
+//  VelogOnMobile
+//
+//  Created by 홍준혁 on 2023/05/11.
+//
+
+import UIKit
+
+final class OnBoardingView: BaseUIView {
+    
+}
+
---
+++ VelogOnMobile/Presentation/OnBoarding/ViewController/OnBoardingViewController.swift
@@ -0,0 +1,12 @@
+//
+//  OnBoardingViewController.swift
+//  VelogOnMobile
+//
+//  Created by 홍준혁 on 2023/05/11.
+//
+
+import Foundation
+
+final class OnBoardingViewController: BaseViewController {
+    
+}
