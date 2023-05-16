fc28f911efaea154b63b878918ab940b000e1072
JuHyeonAh <jha08130930@gmail.com>
2023-05-16 18:13:11 +0900

    로그인, 회원가입 UI 1차 개발

--- VelogOnMobile/Presentation/SignIn/SignInViewController.swift
+++ VelogOnMobile/Presentation/SignIn/SignInViewController.swift
@@ -4,14 +4,191 @@
 //
 //  Created by 홍준혁 on 2023/04/29.
 //
+//  Created by JuHyeonAh on 2023/05/14.
+//
 
 import UIKit
+import SnapKit
 
 class SignInViewController: UIViewController {
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Login"
+        label.font = UIFont(name: "Apple SD Gothic Neo", size: 30)
+        label.textColor = .black
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let idTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "   아이디"
+        textField.font = UIFont(name: "Apple SD Gothic Neo", size: 14)
+        textField.layer.cornerRadius = 4
+        textField.borderStyle = .roundedRect
+        textField.clearButtonMode = .whileEditing
+        textField.layer.borderColor = UIColor.gray.cgColor
+        textField.layer.borderWidth = 1
+        return textField
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "   비밀번호"
+        textField.font = UIFont(name: "Apple SD Gothic Neo", size: 14)
+        textField.layer.cornerRadius = 4
+        textField.borderStyle = .roundedRect
+        textField.clearButtonMode = .whileEditing
+        textField.layer.borderColor = UIColor.gray.cgColor
+        textField.layer.borderWidth = 1
+        return textField
+    }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("로그인", for: .normal)
+        button.titleLabel?.font = UIFont(name: "Apple SD Gothic Neo", size: 17)
+        button.layer.cornerRadius = 4
+        button.backgroundColor = .init(_colorLiteralRed: 0.118, green: 0.784, blue: 0.592, alpha: 1.0)
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
+    
+    private lazy var signupButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("회원가입", for: .normal)
+        button.titleLabel?.font = UIFont(name: "Apple SD Gothic Neo", size: 13)
+        button.backgroundColor = .white
+        button.setTitleColor(.darkGray, for: .normal)
+        button.addTarget(self,
+                         action: #selector(signupButtonTapped),
+                         for: .touchUpInside)
+        return button
+    }()
+    
+    private let findidButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("아이디 / 비밀번호 찾기", for: .normal)
+        button.titleLabel?.font = UIFont(name: "Apple SD Gothic Neo", size: 13)
+        button.backgroundColor = .white
+        button.setTitleColor(.darkGray, for: .normal)
+//        button.addTarget(self,
+//                         action: #selector(findidButtonTapped),
+//                         for: .touchUpInside)
+        return button
+    }()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        set()
+        setLayout()
+        
     }
     
+    func moveToSignUpViewController() {
+    
+    let signupViewController = SignUpViewController()
+    signupViewController.modalPresentationStyle = .fullScreen
+    self.present(signupViewController, animated: true)
 }
+//    func moveToFindIDViewController() {
+//
+//    let findidViewController = FindIDViewController()
+//    findidViewController.modalPresentationStyle = .fullScreen
+//    self.present(findidViewController, animated: true)
+//}
+    @objc
+    func signupButtonTapped() {
+        
+        moveToSignUpViewController()
+    }
+    
+//    @objc
+//    func findidButtonTapped() {
+//
+//        moveToFindIDViewController()
+//    }
+
+}
+
+//func moveToFindIDViewController() {
+//
+//let findIDViewConroller = SignupViewController()
+//signupViewController.modalPresentationStyle = .fullScreen
+//self.present(signupViewController, animated: true)
+//}
+//
+//@objc
+//func signupButtonTapped() {
+//
+//    FindIDViewController()
+//}
+
+
+
+private extension SignInViewController {
+    
+    func set() {
+        
+        view.backgroundColor = .white
+        
+    }
+    
+    func setLayout() {
+        
+        view.addSubview(titleLabel)
+        view.addSubview(idTextField)
+        view.addSubview(passwordTextField)
+        view.addSubview(loginButton)
+        view.addSubview(signupButton)
+        view.addSubview(findidButton)
+        
+        titleLabel.snp.makeConstraints{
+            $0.top.equalToSuperview().offset(30)
+            $0.centerX.equalToSuperview()
+        }
+        
+        idTextField.snp.makeConstraints{
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(titleLabel.snp.bottom).offset(30)
+            $0.leading.equalToSuperview().offset(60)
+            $0.trailing.equalToSuperview().offset(-60)
+            $0.height.equalTo(40)
+        }
+        
+        passwordTextField.snp.makeConstraints{
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(idTextField.snp.bottom).offset(5)
+            $0.leading.equalToSuperview().offset(60)
+            $0.trailing.equalToSuperview().offset(-60)
+            $0.height.equalTo(40)
+        }
+        
+        loginButton.snp.makeConstraints {
+            $0.top.equalTo(passwordTextField.snp.bottom).offset(20)
+            $0.leading.equalToSuperview().offset(60)
+            $0.trailing.equalToSuperview().offset(-59)
+            $0.height.equalTo(46)
+
+        }
+        
+        signupButton.snp.makeConstraints {
+            $0.top.equalTo(loginButton.snp.bottom).offset(15)
+            $0.leading.equalToSuperview().offset(115)
+            $0.bottom.equalToSuperview().offset(-340)
+        }
+        findidButton.snp.makeConstraints {
+            $0.top.equalTo(loginButton.snp.bottom).offset(15)
+            $0.leading.equalTo(signupButton.snp.trailing).offset(20)
+            $0.bottom.equalToSuperview().offset(-340)
+        }
+
+        
+    }
+}
+
+
+
--- VelogOnMobile/Presentation/SignUp/SignUpViewController.swift
+++ VelogOnMobile/Presentation/SignUp/SignUpViewController.swift
@@ -4,26 +4,257 @@
 //
 //  Created by 홍준혁 on 2023/04/29.
 //
+//  Velog_Signview
+//
+//  Created by JuHyeonAh on 2023/05/14.
+//
 
 import UIKit
+import SnapKit
 
 class SignUpViewController: UIViewController {
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Sign up"
+        label.font = UIFont(name: "Apple SD Gothic Neo", size: 25)
+        label.textColor = .black
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "ㆍ이름"
+        label.font = UIFont(name: "Apple SD Gothic Neo", size: 13)
+        label.textColor = .black
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let emailLabel: UILabel = {
+        let label = UILabel()
+        label.text = "ㆍ이메일"
+        label.font = UIFont(name: "Apple SD Gothic Neo", size: 13)
+        label.textColor = .black
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let pwLabel: UILabel = {
+        let label = UILabel()
+        label.text = "ㆍ 비밀번호"
+        label.font = UIFont(name: "Apple SD Gothic Neo", size: 13)
+        label.textColor = .black
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let pwcheckLabel: UILabel = {
+        let label = UILabel()
+        label.text = "ㆍ 비밀번호 확인"
+        label.font = UIFont(name: "Apple SD Gothic Neo", size: 13)
+        label.textColor = .black
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let nameTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = " 이름"
+        textField.font = UIFont(name: "Apple SD Gothic Neo", size: 13)
+        textField.clearButtonMode = .whileEditing
+        textField.layer.borderColor = UIColor.gray.cgColor
+        textField.layer.borderWidth = 1
+        textField.layer.cornerRadius = 4
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    
+    private let emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = " 이메일"
+        textField.font = UIFont(name: "Apple SD Gothic Neo", size: 13)
+        textField.clearButtonMode = .whileEditing
+        textField.layer.borderColor = UIColor.gray.cgColor
+        textField.layer.borderWidth = 1
+        textField.layer.cornerRadius = 4
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    
+    private let pwTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = " 비밀번호"
+        textField.font = UIFont(name: "Apple SD Gothic Neo", size: 13)
+        textField.clearButtonMode = .whileEditing
+        textField.layer.borderColor = UIColor.gray.cgColor
+        textField.layer.borderWidth = 1
+        textField.layer.cornerRadius = 4
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    
+    private let pwcheckTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = " 비밀번호 확인"
+        textField.font = UIFont(name: "Apple SD Gothic Neo", size: 13)
+        textField.clearButtonMode = .whileEditing
+        textField.layer.borderColor = UIColor.gray.cgColor
+        textField.layer.borderWidth = 1
+        textField.layer.cornerRadius = 4
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    
+    private lazy var signupButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("완료", for: .normal)
+        button.titleLabel?.font = UIFont(name: "Apple SD Gothic Neo", size: 18)
+        button.backgroundColor = .init(_colorLiteralRed: 0.118, green: 0.784, blue: 0.592, alpha: 1.0)
+        button.setTitleColor(.white, for: .normal)
+        
+        button.addTarget(self,
+                         action: #selector(signupcomButtonTapped),
+                         for: .touchUpInside)
+        return button
+    }()
+    
+    let stackViewSignup: UIStackView = {
+        
+        let stackView = UIStackView()
+
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.spacing = 10
+        stackView.distribution = .equalSpacing
+        
+        return stackView
+    }()
+    
+    let stackViewSignUp: UIStackView = {
+        let stackView = UIStackView()
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.spacing = 26
+        stackView.distribution = .equalSpacing
+        
+        return stackView
+    }()
+
+
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        set()
+        setLayout()
+        
+    }
+    
+    func backToSigninViewController() {
+            
+            let signinViewController = SignInViewController()
+            signinViewController.modalPresentationStyle = .fullScreen
+            self.present(signinViewController, animated: true)
+        }
+    
+    @objc
+    func signupcomButtonTapped() {
+        
+        backToSigninViewController()
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+
+
+private extension SignUpViewController {
+    
+    func set() {
+        
+        view.backgroundColor = .white
     }
-    */
-
+    
+    func setLayout() {
+        
+        view.addSubview(titleLabel)
+        view.addSubview(nameLabel)
+        view.addSubview(nameTextField)
+        view.addSubview(emailLabel)
+        view.addSubview(emailTextField)
+        view.addSubview(pwLabel)
+        view.addSubview(pwTextField)
+        view.addSubview(pwcheckLabel)
+        view.addSubview(pwcheckTextField)
+        view.addSubview(signupButton)
+        
+        titleLabel.snp.makeConstraints{
+            $0.top.equalToSuperview().offset(100)
+            $0.leading.equalToSuperview().offset(146)
+            $0.trailing.equalToSuperview().offset(-146)
+        }
+        
+        nameLabel.snp.makeConstraints{
+            $0.top.equalTo(titleLabel.snp.bottom).offset(70)
+            $0.leading.equalToSuperview().offset(38)
+            $0.height.equalTo(16)
+        }
+        
+        nameTextField.snp.makeConstraints{
+            $0.top.equalTo(nameLabel.snp.bottom).offset(15)
+            $0.leading.equalToSuperview().offset(38)
+            $0.trailing.equalToSuperview().offset(-38)
+            $0.height.equalTo(45)
+        }
+        
+        emailLabel.snp.makeConstraints{
+            $0.top.equalTo(nameTextField.snp.bottom).offset(30)
+            $0.leading.equalToSuperview().offset(38)
+            $0.height.equalTo(16)
+        }
+        
+        emailTextField.snp.makeConstraints{
+            $0.top.equalTo(emailLabel.snp.bottom).offset(15)
+            $0.leading.equalToSuperview().offset(38)
+            $0.trailing.equalToSuperview().offset(-38)
+            $0.height.equalTo(45)
+        }
+        pwLabel.snp.makeConstraints{
+            $0.top.equalTo(emailTextField.snp.bottom).offset(30)
+            $0.leading.equalToSuperview().offset(38)
+            $0.height.equalTo(16)
+        }
+        
+        pwTextField.snp.makeConstraints{
+            $0.top.equalTo(pwLabel.snp.bottom).offset(15)
+            $0.leading.equalToSuperview().offset(38)
+            $0.trailing.equalToSuperview().offset(-38)
+            $0.height.equalTo(45)
+        }
+        pwcheckLabel.snp.makeConstraints{
+            $0.top.equalTo(pwTextField.snp.bottom).offset(30)
+            $0.leading.equalToSuperview().offset(38)
+            $0.height.equalTo(16)
+        }
+        pwcheckTextField.snp.makeConstraints{
+            $0.top.equalTo(pwcheckLabel.snp.bottom).offset(15)
+            $0.leading.equalToSuperview().offset(38)
+            $0.trailing.equalToSuperview().offset(-38)
+            $0.height.equalTo(45)
+        }
+        
+        signupButton.snp.makeConstraints{
+            $0.top.equalTo(pwcheckTextField.snp.bottom).offset(60)
+            $0.leading.equalToSuperview().offset(38)
+            $0.trailing.equalToSuperview().offset(-38)
+            $0.height.equalTo(46)
+        }
+        
+    }
 }
