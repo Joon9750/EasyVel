3719500e1d6ab1ea75ddfb96903e69e6c8beecfc
Hongjunehuke <junhyeok2166@daum.net>
2023-05-19 20:27:25 +0900

    [CHORE] SizeLiterals, Post cell 사이즈 수정

--- VelogOnMobile/Global/Literal/SizeLiterals.swift
+++ VelogOnMobile/Global/Literal/SizeLiterals.swift
@@ -8,8 +8,8 @@
 import UIKit
 
 enum SizeLiterals {
-    static let postCellSmall: CGFloat = 150
-    static let postCellMedium: CGFloat = 160
+    static let postCellSmall: CGFloat = 160
+    static let postCellMedium: CGFloat = 170
     static let postCellLarge: CGFloat = 180
     static let postCellXLarge: CGFloat = 280
 }
