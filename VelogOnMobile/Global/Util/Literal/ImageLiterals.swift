c20770c4ac15899c3d7493d61b40fff664bddf33
Hongjunehuke <junhyeok2166@daum.net>
2023-05-25 00:46:01 +0900

    [ADD] Assets 추가

--- VelogOnMobile/Global/Util/Literal/ImageLiterals.swift
+++ VelogOnMobile/Global/Util/Literal/ImageLiterals.swift
@@ -8,6 +8,16 @@
 import UIKit
 
 enum ImageLiterals {
+    
+    static var settingTabIcon: UIImage { .load(name: "settingIcon") }
+    static var saveBookMarkIcon: UIImage { .load(name: "bookmarkSave") }
+    static var unSaveBookMarkIcon: UIImage { .load(name: "bookmarkUnsave") }
+    static var homeTabIcon: UIImage { .load(name: "homeIcon") }
+    static var listTabIcon: UIImage { .load(name: "listIcon") }
+    static var searchIcon: UIImage { .load(name: "search") }
+    static var plusFolder: UIImage { .load(name: "plusFolder") }
+    
+    
     static var emptyAlarm: UIImage { .load(name: "emptyAlarm") }
     static var emptyPostsList: UIImage { .load(name: "emptyPostsList") }
     static var emptyKeywordList: UIImage { .load(name: "emptyKeywordList") }
