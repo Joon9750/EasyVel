514d5c8001ec18f2ae288a7db82326e5a9a44eee
Hongjunehuke <junhyeok2166@daum.net>
2023-05-27 03:43:19 +0900

    [Refact] WebViewController, LoadingView 구현

--- VelogOnMobile/Feature/WebView/ViewController/WebViewController.swift
+++ VelogOnMobile/Feature/WebView/ViewController/WebViewController.swift
@@ -13,7 +13,7 @@ import RxSwift
 
 final class WebViewController: RxBaseViewController<WebViewModel> {
     
-    let webView : WKWebView = {
+    lazy var webView : WKWebView = {
         let prefs = WKWebpagePreferences()
         prefs.allowsContentJavaScript = true
         let configuration = WKWebViewConfiguration()
@@ -22,15 +22,29 @@ final class WebViewController: RxBaseViewController<WebViewModel> {
         return webView
     }()
     
+    private let webViewProgressRelay = PublishRelay<Double>()
+    
     override func render() {
-        self.view = webView
+        view = webView
     }
-
+    
     override func bind(viewModel: WebViewModel) {
         super.bind(viewModel: viewModel)
         bindOutput(viewModel)
+        
+        webView.rx.observe(Double.self, "estimatedProgress")
+            .compactMap { $0 }
+            .distinctUntilChanged()
+            .bind(to: webViewProgressRelay)
+            .disposed(by: disposeBag)
+        
+        webViewProgressRelay
+            .subscribe(onNext: { [weak self] progress in
+                self?.updateLoadingIndicator(progress: progress)
+            })
+            .disposed(by: disposeBag)
     }
-
+    
     private func bindOutput(_ viewModel: WebViewModel) {
         viewModel.urlRequest
             .asDriver(onErrorJustReturn: URLRequest(url: URL(fileURLWithPath: "")))
@@ -43,5 +57,13 @@ final class WebViewController: RxBaseViewController<WebViewModel> {
     override func setupNavigationBar() {
         navigationController?.navigationBar.tintColor = .black
         navigationController?.navigationBar.topItem?.title = TextLiterals.noneText
+    }
+    
+    func updateLoadingIndicator(progress: Double) {
+        if progress < 0.8 {
+            LoadingView.showLoading()
+        } else {
+            LoadingView.hideLoading()
+        }
     }
 }
