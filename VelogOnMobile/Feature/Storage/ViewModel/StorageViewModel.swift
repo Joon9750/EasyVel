a8d5dcd4a1fabbbd2fccbeec05df89c6dcc9d19b
Hongjunehuke <junhyeok2166@daum.net>
2023-05-28 18:23:08 +0900

    [REFACT] Storage, Rx refact

--- VelogOnMobile/Feature/Storage/ViewModel/StorageViewModel.swift
+++ VelogOnMobile/Feature/Storage/ViewModel/StorageViewModel.swift
@@ -8,64 +8,56 @@
 import Foundation
 
 import RealmSwift
+import RxRelay
+import RxSwift
 
-protocol StorageViewModelInput {
-    
-    // MARK: - fix me
-    func deletePostButtonDidTap(url: String)
-    func viewWillAppear()
-}
-
-protocol StorageViewModelOutput {
-    var storagePosts: (([StoragePost]) -> Void)? { get set }
-    var isPostsEmpty: ((Bool) -> Void)? { get set }
-}
-
-protocol StorageViewModelInputOutput: StorageViewModelInput, StorageViewModelOutput {}
-
-final class StorageViewModel: StorageViewModelInputOutput {
+final class StorageViewModel: BaseViewModel {
 
     let realm = RealmService()
-    private var posts = [StoragePost]()
 
     // MARK: - Input
     
-    func deletePostButtonDidTap(url: String) {
-        deletePostInRealm(url: url)
-        getPostInRealm()
-        if let isPostsEmpty = isPostsEmpty {
-            isPostsEmpty(checkStorageEmpty(storage: posts))
-        }
-    }
-    
-    func viewWillAppear() {
-        getPostInRealm()
-        if let isPostsEmpty = isPostsEmpty {
-            isPostsEmpty(checkStorageEmpty(storage: posts))
-        }
-    }
+    var deletePostButtonDidTap = PublishRelay<String>()
     
     // MARK: - Output
+    
+    var storagePostsOutput = PublishRelay<[StoragePost]>()
+    var isPostsEmptyOutput = PublishRelay<Bool>()
+    
+    override init() {
+        super.init()
+        makeOutput()
+    }
+    
+    private func makeOutput() {
+        viewWillAppear
+            .subscribe(onNext: { [weak self] in
+                let realmData = self?.getPostInRealm()
+                self?.storagePostsOutput.accept(realmData ?? [StoragePost]())
+                let isEmpty = self?.checkStorageEmpty(storage: realmData ?? [StoragePost]())
+                self?.isPostsEmptyOutput.accept(isEmpty ?? Bool())
+            })
+            .disposed(by: disposeBag)
         
-    var storagePosts: (([StoragePost]) -> Void)?
-    var isPostsEmpty: ((Bool) -> Void)?
+        deletePostButtonDidTap
+            .subscribe(onNext: { [weak self] url in
+                self?.realm.deletePost(url: url)
+                let realmData = self?.getPostInRealm()
+                let isEmpty = self?.checkStorageEmpty(storage: realmData ?? [StoragePost]())
+                self?.isPostsEmptyOutput.accept(isEmpty ?? Bool())
+            })
+            .disposed(by: disposeBag)
+    }
     
     // MARK: - func
     
-    private func getPostInRealm() {
+    private func getPostInRealm() -> [StoragePost] {
         let realmPostData = realm.getPosts()
         let posts: [StoragePost] = realm.convertToStoragePost(input: realmPostData)
         let reversePosts = realm.reversePosts(input: posts)
-        if let storagePosts = storagePosts {
-            storagePosts(reversePosts)
-        }
-        self.posts = reversePosts
+        return reversePosts
     }
-    
-    private func deletePostInRealm(url: String) {
-        realm.deletePost(url: url)
-    }
-    
+
     private func checkStorageEmpty(storage: [StoragePost]) -> Bool {
         if storage.count == 0 { return true }
         else { return false }
