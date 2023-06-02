d2adce4cab7d857ca40ea58270238edc98a0df77
Melt <seokwoo2000@naver.com>
2023-06-02 18:02:57 +0900

    [ADD] Font Extension 추가

---
+++ VelogOnMobile/Global/Util/Extension/UIFont+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UIFont+Extension.swift
+//  VelogOnMobile
+//
+//  Created by 장석우 on 2023/06/02.
+//
+
+import UIKit
+
+extension UIFont {
+    static func appleGothicNeo(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "Apple SD Gothic Neo", size: size)!
+    }
+}
