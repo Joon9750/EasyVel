c4119b3bc46ff0d114e50706d2c71bb719f54681
Hongjunehuke <junhyeok2166@daum.net>
2023-06-14 15:17:11 +0900

    [CHORE] 글 검색 뷰 수정

--- VelogOnMobile/Feature/Search/AllSearch/SearchCollectionViewCell.swift
+++ VelogOnMobile/Feature/Search/AllSearch/SearchCollectionViewCell.swift
@@ -14,42 +14,26 @@ final class SearchCollectionViewCell: BaseCollectionViewCell {
     
     private let searchwordLabel : UILabel = {
         let label = UILabel()
-        
-        label.font = UIFont(name: "Apple SD Gothic Neo", size: 20)
-        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.font = UIFont(name: "Avenir-Black", size: 12)
         label.textAlignment = .center
         label.textColor = .black
-        label.layer.borderWidth = 1.5
-        label.layer.borderColor = UIColor.gray.cgColor
+        label.layer.borderWidth = 1.0
+        label.layer.borderColor = UIColor.lightGray.cgColor
         label.layer.cornerRadius = 10.0
         label.layer.masksToBounds = true
         return label
     }()
     
-    
-    override init(frame: CGRect) {
-           super.init(frame: frame)
-    }
-
-    @available(*, unavailable)
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     override func render() {
-        
         contentView.addSubview(searchwordLabel)
         
         searchwordLabel.snp.makeConstraints {
-            $0.leading.equalToSuperview().offset(3)
-            $0.trailing.equalToSuperview().offset(-3)
+            $0.leading.trailing.equalToSuperview().inset(3)
             $0.height.equalTo(25)
         }
-        
     }
     
     func configCell(_ trend: Trend) {
-        
         searchwordLabel.text = trend.keyword
     }
 }
