094d3ea86c08418a52b39bf642c943603076f9e4
Hongjunehuke <junhyeok2166@daum.net>
2023-06-18 15:13:56 +0900

    [ADD] Setting view UI 변경사항 적용

--- VelogOnMobile/Feature/Setting/View/SettingView.swift
+++ VelogOnMobile/Feature/Setting/View/SettingView.swift
@@ -10,28 +10,12 @@ import UIKit
 import SnapKit
 
 final class SettingView: BaseUIView {
+    
     private let title: UILabel = {
         let label = UILabel()
         label.text = TextLiterals.settingTitleLabelText
-        label.font = UIFont(name: "Avenir-Black", size: 30)
+        label.font = UIFont(name: "Avenir-Black", size: 24)
         return label
-    }()
-
-    private let alertText: UILabel = {
-        let label = UILabel()
-        label.text = TextLiterals.settingAlertText
-        return label
-    }()
-    
-    private let alertSwitch: UISwitch = {
-        let alertSwitch = UISwitch()
-        return alertSwitch
-    }()
-    
-    private let grayView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .grayColor
-        return view
     }()
     
     private let tableView = SettingTableView()
@@ -39,35 +23,16 @@ final class SettingView: BaseUIView {
     override func render() {
         self.addSubviews(
             title,
-            alertText,
-            alertSwitch,
-            grayView,
             tableView
         )
         
         title.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(60)
+            $0.top.equalToSuperview().offset(76)
             $0.leading.equalToSuperview().offset(20)
         }
-        
-        alertText.snp.makeConstraints {
-            $0.top.equalTo(title.snp.bottom).offset(62)
-            $0.leading.equalToSuperview().offset(35)
-        }
-        
-        alertSwitch.snp.makeConstraints {
-            $0.trailing.equalToSuperview().inset(20)
-            $0.centerY.equalTo(alertText)
-        }
-        
-        grayView.snp.makeConstraints {
-            $0.top.equalTo(alertText.snp.bottom).offset(16)
-            $0.leading.trailing.equalToSuperview()
-            $0.height.equalTo(10)
-        }
-        
+
         tableView.snp.makeConstraints {
-            $0.top.equalTo(grayView.snp.bottom)
+            $0.top.equalTo(title.snp.bottom).offset(24)
             $0.leading.trailing.equalToSuperview()
             $0.height.equalTo(220)
         }
