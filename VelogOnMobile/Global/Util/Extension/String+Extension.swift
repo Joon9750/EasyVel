a03141fff9d99ff8bb0d4f65edd1bbe1f967bccb
Hongjunehuke <junhyeok2166@daum.net>
2023-07-02 07:34:15 +0900

    [CHORE] 줄바꿈 정리

--- VelogOnMobile/Global/Util/Extension/String+Extension.swift
+++ VelogOnMobile/Global/Util/Extension/String+Extension.swift
@@ -31,18 +31,12 @@ extension String {
             return false
         }
     }
-}
-
-
-extension String {
+    
     /// Label안에 Image를 넣을때 적절한 String타입으로 만들어주는 함수
     /// - Returns: Label을 리턴해줌
     func makeNSAttributedString() -> NSAttributedString {
         return NSAttributedString(string: self)
     }
-}
-
-extension String {
     
     /// String의 너비를 반환해주는 함수
     /// - Parameter addPadding: padding값을 더해줄때 사용(기본값 0)
@@ -55,4 +49,3 @@ extension String {
         return label.frame.width + addPadding
     }
 }
-
