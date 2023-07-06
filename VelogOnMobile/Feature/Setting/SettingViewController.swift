214f39bf611590ca6a19231223f2f1ed3360566c
Melt <seokwoo2000@naver.com>
2023-07-07 06:03:49 +0900

    [FEAT] #200 - 로그아웃시 DB accessToken 삭제 로직 구현

--- VelogOnMobile/Feature/Setting/SettingViewController.swift
+++ VelogOnMobile/Feature/Setting/SettingViewController.swift
@@ -52,7 +52,10 @@ final class SettingViewController: RxBaseViewController<SettingViewModel> {
                 if didSuccess {
                     self?.pushToSignInView()
                 } else {
+                    
                     // MARK: - 회원탈퇴 실패
+                    self?.showToast(toastText: "회원탈퇴에 실패하였습니다.",
+                                   backgroundColor: .gray200)
                 }
             })
             .disposed(by: disposeBag)
